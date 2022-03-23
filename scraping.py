0dbc0d06e2beb98c80439309a51f5ef54f1b429b
Brittany Marchand <94507822+blmarcha7@users.noreply.github.com>
2022-03-22 20:46:07 -0400

    updating files

--- scraping.py
+++ scraping.py
@@ -21,10 +21,10 @@ def scrape_all():
     data = {
           "news_title": news_title,
           "news_paragraph": news_paragraph,
-          "featured_image": featured_image(),
+          "featured_image": featured_image(browser),
           "facts": mars_facts(),
           "last_modified": dt.datetime.now(),
-          "hemispheres_images":mars_images()
+          "hemispheres_images":mars_images(browser)
     }
     # Stop webdriver and return data
     browser.quit()
@@ -57,7 +57,7 @@ def mars_news(browser):
 
     return news_title, news_p
 
-def featured_image():
+def featured_image(browser):
     # Visit URL
     url = 'https://spaceimages-mars.com'
     browser.visit(url)
@@ -99,7 +99,7 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
 
-def mars_images():
+def mars_images(browser):
     # 1. Use browser to visit the URL 
     base_url = 'https://marshemispheres.com/'
 
